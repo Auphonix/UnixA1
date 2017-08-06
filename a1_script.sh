c9484a5fc184d64dcb9bde32fa347bf26f21f248
Danyon <matchstick_dan@hotmail.com>
2017-08-06 14:08:46 +1000

    Added resource directory, common_pass.txt and db_data.txt. Also added file check in a1_script.sh

--- a1_script.sh
+++ a1_script.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+# See if the specified files existt
+db_date = "db_data.txt"
+common_pass = "common_pass.txt"
+r_path = "resources/" # Resource path
+
+
+
+echo 'Program checking files exist...'
+if [-f $r_path$db_data]; then
+    echo "File <$db_data> exists"
+else
+    echo "File <$db_data> doesn't exist"
+fi
---
+++ resources/readme.txt
@@ -0,0 +1,9 @@
+This is the project folder for Unix Administration Assignment 1.
+
+
+
+
+
+Resources used:
+
+common_pass.txt -> https://github.com/danielmiessler/SecLists/blob/master/Passwords/10_million_password_list_top_1000000.txt
